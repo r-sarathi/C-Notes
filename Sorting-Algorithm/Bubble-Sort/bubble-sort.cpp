18dae8f150f8561da67e37602b7fc11aad54a3e8
SARATHI R <sarathirofficial@gmail.com>
2024-08-30 17:07:00 +0530

    Bubble-Sort

---
+++ Sorting-Algorithm/Bubble-Sort/bubble-sort.cpp
@@ -0,0 +1,11 @@
+/*
+
+Bubble Sort:
+    - It is the simplest sorting algorithm that works by repeatedly swapping the adjacent
+    elements if they are in the wrong order. This algorithm is not suitable for large data sets
+    as its average and worst-case time complexity is quite high.
+
+Time Complexity = O(N^2)
+Space Complexity = O(1)
+
+*/
