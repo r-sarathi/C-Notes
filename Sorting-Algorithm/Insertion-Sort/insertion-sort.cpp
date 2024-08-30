11f0f66b3037ea865fc79bc80c2c68677aa8be87
SARATHI R <sarathirofficial@gmail.com>
2024-08-30 17:41:59 +0530

    Insertion-Sort

---
+++ Sorting-Algorithm/Insertion-Sort/insertion-sort.cpp
@@ -0,0 +1,15 @@
+/*
+
+Insertion Sort:
+    - It is a simple sorting algorithm that works by building a sorted array one element
+    at a time. It is considered an "in-place" sorting algorithm, meaning it doesn't require
+    any additional memory space beyond teh original array.
+
+Time Complexity:
+    - Best Case: O(n)
+    - Average Case: O(n^2)
+    - Worst Case: O(n^2)
+
+Space Complexity =  O(1)
+
+*/
