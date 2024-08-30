b3065a821040658a4ddb30cf56ec074f661b1fb4
SARATHI R <sarathirofficial@gmail.com>
2024-08-30 17:46:16 +0530

    Insertion Sort - Code updated

--- Sorting-Algorithm/Insertion-Sort/insertion-sort.cpp
+++ Sorting-Algorithm/Insertion-Sort/insertion-sort.cpp
@@ -13,3 +13,41 @@ Time Complexity:
 Space Complexity =  O(1)
 
 */
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    cout << "After sorting an array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {23, 13, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    return 0;
+}
+
+/*
+
+Output:
+After sorting an array: 1 2 3 13 23
+
+*/
